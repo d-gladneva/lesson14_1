3a6faf2f7b4f1dd9c5c943f38fde41dcaae149a4
Dasha <davydova_di@mail.ru>
2020-09-07 22:12:50 +0300

    fix: conditions|script.js

--- lesson14_1.js
+++ lesson14_1.js
@@ -14,12 +14,13 @@ DomElement.prototype.createELem = function () {
 
         const div = document.createElement('div');
         document.body.append(div);
+        div.style.selector = this.selector;
         div.style.height = this.height;
         div.style.width = this.width;
         div.style.background = this.bg;
         div.style.fontSize = this.fontSize;
-        div.style.position = this.position;
         div.textContent = this.text;
+        div.style.position = this.position;
         div.classList.add('block');
     } else if (this.selector.charAt(0) === '#') {
         const p = document.createElement('p');
@@ -27,28 +28,16 @@ DomElement.prototype.createELem = function () {
         p.style.fontSize = this.fontSize;
         p.textContent = this.text;
         p.id = 'best';
-    } else if (this.selector.charAt(0) === '1') {
-        const div = document.createElement('div');
-        document.body.append(div);
-        div.style.height = this.height;
-        div.style.width = this.width;
-        div.style.background = this.bg;
-        div.style.fontSize = this.fontSize;
-        div.style.position = this.position;
-        div.textContent = this.text;
-        div.classList.add('second');
+    } else  {
+        console.log('Ошибка! Неверно переданы параметры.')
     }
 };
 
-let elem = new DomElement('.block', '50px', '50px', 'red', '18px', 'blabla');
-let square = new DomElement('#block', '100px', '100px', 'green', 'absolute');
-let sqr = new DomElement('1block', '100px', '100px', 'blue', 'absolute');
+let sqr = new DomElement('#block', '50px', '50px', 'red', '18px', 'blabla', 'absolute');
 
-elem.createELem();
-square.createELem();
 sqr.createELem();
 
-const second = document.getElementsByClassName('second')[0];
+const second = document.getElementsByClassName('block')[0];
 let coordY = 0;
 let coordX = 0;
 document.addEventListener('keydown', function (e) {
