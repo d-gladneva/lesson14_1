a04e8c8ffb3b3765a69d93246a1f2933f984871d
Dasha <davydova_di@mail.ru>
2020-09-07 13:54:54 +0300

    fix: logic in createElem

--- lesson14_1.js
+++ lesson14_1.js
@@ -1,25 +1,32 @@
-function DomElement(selector, height, width, bg, fontSize) {
+function DomElement(selector, height, width, bg, fontSize, text) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
+    this.text = text;
+
 }
 
 DomElement.prototype.createELem = function () {
     if (this.selector === '.block') {
         const div = document.createElement('div');
         document.body.append(div);
-        div.textContent = 'block';
+        div.style.height = this.height;
+        div.style.width = this.width;
+        div.style.background = this.bg;
+        div.style.fontSize = this.fontSize;
+        div.textContent = this.text;
         div.classList.add('block');
     } else if (this.selector === '#best') {
         const p = document.createElement('p');
         document.body.append(p);
-        p.textContent = 'best';
+        p.style.fontSize = this.fontSize;
+        p.textContent = this.text;
         p.id = 'best';
     }
 };
 
-let elem = new DomElement('#best', '50px', '50px', '#000', '18px');
+let elem = new DomElement('.block', '50px', '50px', 'red', '18px', 'blabla');
 
 elem.createELem();
