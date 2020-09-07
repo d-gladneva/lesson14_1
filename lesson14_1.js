07a14ee69fa96c39de5ec40fbb29530b645bea93
Dasha <davydova_di@mail.ru>
2020-09-07 19:37:19 +0300

    fix: condition about selector, feat: second sqr|script.js

--- lesson14_1.js
+++ lesson14_1.js
@@ -1,32 +1,73 @@
-function DomElement(selector, height, width, bg, fontSize, text) {
+function DomElement(selector, height, width, bg, fontSize, text, position) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
     this.text = text;
-
+    this.position = position;
 }
 
 DomElement.prototype.createELem = function () {
-    if (this.selector === '.block') {
+    console.log(this.selector.charAt(0));
+    if (this.selector.charAt(0) === '.') {
+
         const div = document.createElement('div');
         document.body.append(div);
         div.style.height = this.height;
         div.style.width = this.width;
         div.style.background = this.bg;
         div.style.fontSize = this.fontSize;
+        div.style.position = this.position;
         div.textContent = this.text;
         div.classList.add('block');
-    } else if (this.selector === '#best') {
+    } else if (this.selector.charAt(0) === '#') {
         const p = document.createElement('p');
         document.body.append(p);
         p.style.fontSize = this.fontSize;
         p.textContent = this.text;
         p.id = 'best';
+    } else if (this.selector.charAt(0) === '1') {
+        const div = document.createElement('div');
+        document.body.append(div);
+        div.style.height = this.height;
+        div.style.width = this.width;
+        div.style.background = this.bg;
+        div.style.fontSize = this.fontSize;
+        div.style.position = this.position;
+        div.textContent = this.text;
+        div.classList.add('second');
     }
 };
 
 let elem = new DomElement('.block', '50px', '50px', 'red', '18px', 'blabla');
+let square = new DomElement('#block', '100px', '100px', 'green', 'absolute');
+let sqr = new DomElement('1block', '100px', '100px', 'blue', 'absolute');
 
 elem.createELem();
+square.createELem();
+sqr.createELem();
+
+const second = document.getElementsByClassName('second')[0];
+let coordY = 0;
+let coordX = 0;
+document.addEventListener('keydown', function (e) {
+    if (e.code === 'ArrowLeft') {
+        coordX -= 10;
+        second.style.marginLeft = coordX + 'px';
+        console.log('left');
+    } else if (e.code === 'ArrowDown') {
+        coordY += 10;
+        second.style.marginTop = coordY + 'px';
+        console.log('down');
+    } else if (e.code === 'ArrowUp') {
+        coordY -= 10;
+        second.style.marginTop = coordY + 'px';
+        console.log('up');
+    } else if (e.code === 'ArrowRight') {
+        coordX += 10;
+        second.style.marginLeft = coordX + 'px';
+        console.log('right');
+    }
+
+});
